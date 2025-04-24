f2e1555d906c8dda8ce7e7290716f1107478e9c0
Jameson Turner <163557480+JamesonTM@users.noreply.github.com>
2025-04-24 10:47:48 -0500

    Updated to LibDemo2

---
+++ LibDemo2/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace LibDemo2
+{
+    public class LibClass2
+    {
+        private string _message;
+        public LibClass2(string message) 
+        {
+            _message = message;
+        }
+        public string GetMessage()
+        {
+            return $"{_message} from Library2";
+        }
+    }
+}
