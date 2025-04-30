d12b8ff67f65b541037305914ee66db0e06fc038
Jameson Turner <163557480+JamesonTM@users.noreply.github.com>
2025-04-30 10:20:16 -0500

    Updated csproj version and add package ref and Class1 to include LibDemo4

--- LibDemo2/Class1.cs
+++ LibDemo2/Class1.cs
@@ -1,15 +1,23 @@
-﻿namespace LibDemo2
+﻿﻿using LibDemo4;
+namespace LibDemo2
 {
     public class LibClass2
     {
         private string _message;
         public LibClass2(string message) 
         {
-            _message = message;
+            _message = Lib(message);
         }
-        public string GetMessage()
+		
+		static string Lib(string txt)
+		{
+			LibClass4 Lib = new LibClass4(txt);
+			return Lib.GetMessage4();			
+		}
+		
+        public string GetMessage2()
         {
-            return $"{_message} from Library2";
+            return $"{_message} and from Library2";
         }
     }
 }
